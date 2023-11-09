07b6aae7ae0a0a7e00b78ba4211aded9793e0581
Niko.Knez <info@design-nk.de>
2023-11-09 15:50:50 +0100

    API

--- admin/class-job-importer-admin.php
+++ admin/class-job-importer-admin.php
@@ -48,27 +48,36 @@ class Job_Importer_Admin
     );
 
     add_settings_field(
-      'job_importer_api_key',
-      'API Schlüssel',
-      array($this, 'api_key_field_callback'),
+      'job_importer_client_id',
+      'Client ID',
+      array($this, 'client_id_field_callback'),
       'job-importer',
       'job_importer_settings_section'
     );
 
-    // Weitere Einstellungsfelder können hier hinzugefügt werden
+    add_settings_field(
+      'job_importer_client_secret',
+      'Client Secret',
+      array($this, 'client_secret_field_callback'),
+      'job-importer',
+      'job_importer_settings_section'
+    );
   }
 
-  public function settings_section_callback()
-  {
-    echo '<p>Hier können Sie die Einstellungen für den Job Importer konfigurieren.</p>';
-  }
-
-  public function api_key_field_callback()
+  public function client_id_field_callback()
   {
     $options = get_option('job_importer_settings');
-    $api_key = isset($options['api_key']) ? esc_attr($options['api_key']) : '';
-    echo '<input type="text" id="job_importer_api_key" name="job_importer_settings[api_key]" value="' . $api_key . '" />';
+    $client_id = isset($options['client_id']) ? esc_attr($options['client_id']) : '';
+    echo '<input type="text" id="job_importer_client_id" name="job_importer_settings[client_id]" value="' . $client_id . '" />';
   }
+
+  public function client_secret_field_callback()
+  {
+    $options = get_option('job_importer_settings');
+    $client_secret = isset($options['client_secret']) ? esc_attr($options['client_secret']) : '';
+    echo '<input type="text" id="job_importer_client_secret" name="job_importer_settings[client_secret]" value="' . $client_secret . '" />';
+  }
+
 
   // Callbacks für zusätzliche Einstellungsfelder können hier hinzugefügt werden
   // Zum Beispiel für einen Client Secret oder ähnliches
