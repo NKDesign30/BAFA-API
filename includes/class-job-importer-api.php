7427382a8d0d299bc430fa4d34401eb187da82dd
Niko.Knez <info@design-nk.de>
2023-11-09 16:11:46 +0100

    API

--- includes/class-job-importer-api.php
+++ includes/class-job-importer-api.php
@@ -1,7 +1,6 @@
 <?php
 class Job_Importer_API
 {
-
   private $client_id;
   private $client_secret;
   private $token_url;
@@ -58,5 +57,36 @@ class Job_Importer_API
 
     $body = wp_remote_retrieve_body($response);
     return json_decode($body);
+  }
+
+  public function import_jobs()
+  {
+    $query_args = []; // Definieren Sie hier die Abfrageparameter
+    $jobs = $this->search_jobs($query_args);
+
+    if ($jobs && is_array($jobs->jobs)) {
+      foreach ($jobs->jobs as $job) {
+        $post_id = wp_insert_post(array(
+          'post_title'    => wp_strip_all_tags($job->title),
+          'post_content'  => $job->description,
+          'post_status'   => 'publish',
+          'post_type'     => 'job_listing',
+          // Weitere Felder wie 'post_author', 'post_date', etc.
+        ));
+
+        if ($post_id) {
+          // Hier könnten Sie zusätzliche Metadaten speichern, z.B.:
+          update_post_meta($post_id, '_job_location', $job->location);
+          update_post_meta($post_id, '_job_company', $job->company);
+          // Weitere Metadaten wie Gehalt, Art der Anstellung, etc.
+        } else {
+          // Fehlerbehandlung
+          error_log('Fehler beim Erstellen des Job-Posts: ' . $job->title);
+        }
+      }
+    } else {
+      // Fehlerbehandlung, wenn keine Jobs gefunden oder ein Fehler aufgetreten ist
+      error_log('Keine Jobs gefunden oder Fehler bei der API-Anfrage.');
+    }
   }
 }
